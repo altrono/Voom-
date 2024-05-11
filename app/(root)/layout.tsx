c78db7369e40ff3129c81ba0f51c62ca32fdf218
Altrono <hbotema16@gmail.com>
2024-05-11 02:05:51 +0200

    Implement Meeting Room

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,16 @@
 
 import React from "react"
 import { StreamVideoProvider } from "../providers/StreamClientProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Voom ",
+  description: "Video Call App",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
 
 const Layout = ({children } : {children: React.ReactNode}) => {
   return (
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,7 +3,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
 import HomeCard from './HomeCard';
 import MeetingModal from './MeetingModal';
 import { useUser } from '@clerk/nextjs';
